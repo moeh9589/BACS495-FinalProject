7b154833207f03f18a6f847cbb954a62b60d2319
Justin Moehlenpah <moeh9589@bears.unco.edu>
2022-01-31 13:02:20 -0700

    Header and Footer

---
+++ finalproject/src/body.js
@@ -0,0 +1,12 @@
+import React from  "react";
+import bg from './images/bg.jpeg';
+
+function Body() {
+    return (
+        <div>
+            <p>Test words</p>
+        </div>
+      )
+}
+
+export default Body
